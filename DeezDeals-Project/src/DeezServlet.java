0159ca4b667a857dba53eaaf9052771bc351f5ba
al <albalanay@unomaha.edu>
2021-08-12 16:59:03 -0500

    servlet updates

--- DeezDeals-Project/src/DeezServlet.java
+++ DeezDeals-Project/src/DeezServlet.java
@@ -1,32 +1,60 @@
+package backend;
+
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.*;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/searchServlet")
 public class DeezServlet extends HttpServlet {
 
-    private String msg;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -6456363214942064149L;
+	
+	
+	public void init(ServletConfig config) {
+		System.out.println("Initializing servlet");
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html> servlet is working </html>");
+		out.flush();
+	}
+	
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    public void init() throws ServletException {
-        msg = "test message";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    	String searchForm = "frontend/index.jsp";
+    	RequestDispatcher dispatcher = request.getRequestDispatcher(searchForm);
+    	dispatcher.forward(request, response);
+    	
+    	String searchresult = request.getParameter("searchinput");
         //response content type
-        response.setContentType("text/html");
+        //response.setContentType("text/html");
 
-        //logic
+        //printwriter to write to html
         PrintWriter out = response.getWriter();
-        out.println("<p>" + msg + "</p>");
+        //out.println("<html>");
+        //out.println("<div>" + searchresult + "</div>");
+        //out.println("test");
+        //out.println("</html>");
+        
+        out.println("<div>" + searchresult + "</div>");
+        //response.sendRedirect("index.jsp?username=" + searchresult);
+        //response.getWriter().write(searchresult);
+        out.println("<html> servlet is working </html>");
 
     }
-
-    public void destroy(){
-        //stuff
+    
+    public void destroy() {
+    	System.out.println("Servlet shutting down");
     }
 }
