6e773aab45da659dc68234311c3d8fd1ca4746ae
jetpack115 <84944487+jetpack115@users.noreply.github.com>
2021-08-14 22:22:46 -0500

    Newest Javadoc

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"ddApp"},{"p":"<Unnamed>","l":"geoLocation"},{"p":"<Unnamed>","l":"twitterAuth"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"ddWebApp"},{"p":"<Unnamed>","l":"DeezServlet"},{"p":"<Unnamed>","l":"fSets"},{"p":"<Unnamed>","l":"geoLocation"},{"p":"<Unnamed>","l":"scraper"},{"p":"<Unnamed>","l":"twitterAuth"}]
